5e8c396fdf22f74ec51736812037d6ea7d4d7dfe
saci zakaria <sacideveloppeur@gmail.com>
2021-10-26 23:12:57 +0200

    AuthMode.Login

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   },
                 ),
                 RaisedButton(
-                  child: Text(""),
+                  child: Text(_authData == AuthMode.Login ?"LOGIN":"SIGN UP"),
                     onPressed: (){}
                     ),
               ],
