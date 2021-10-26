437e33cf7876101892d827997f793b98f222cb61
saci zakaria <sacideveloppeur@gmail.com>
2021-10-26 23:10:42 +0200

    Text

--- lib/main.dart
+++ lib/main.dart
@@ -63,6 +63,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     _authData['email'] = val!;
                   },
                 ),
+                RaisedButton(
+                  child: Text(""),
+                    onPressed: (){}
+                    ),
               ],
             ),
           ),
